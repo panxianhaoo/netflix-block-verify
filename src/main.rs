73a818e4c63e170798b7ece83f923a2014364014
panxianhao <panxianhao@wxchina.com>
2023-10-09 10:44:26 +0800

    调整代码

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use anyhow::Result;
+use colored::Colorize;
 use reqwest::header::USER_AGENT;
 use reqwest::StatusCode;
 use url::Url;
+use netflix_block_verify::get_area_name;
 
 const NETFLIX_ADDR: &str = "https://www.netflix.com/title/";
 const AREA_AVAILABLE_ID: u32 = 80018499;
@@ -13,11 +15,16 @@ async fn main() -> Result<()> {
     let (area, self_made, non_self_made) = tokio::join!(check_is_available(AREA_AVAILABLE_ID),
         check_is_available(SELF_MADE_AVAILABLE_ID),check_is_available(NON_SELF_MADE_AVAILABLE_ID));
     match (area, self_made, non_self_made) {
-        (Some(_), Some(_), Some(_)) => {
-            println!("OK");
+        (Some(area), Some(_), Some(_)) => {
+            println!("{}", "完整解锁，可以看非自制".green());
+            println!("{}{}", "Netflix识别地域为".green(), get_area_name(area).green());
         }
-        (Some(_), Some(_), None) => {
-            println!("只能看自制");
+        (Some(area), Some(_), None) => {
+            println!("{}", "只能看自制".yellow());
+            println!("{}{}", "Netflix识别地域为".yellow(), get_area_name(area).yellow());
+        }
+        (None, None, None) => {
+            println!("{}", "无法观看Netflix".red());
         }
         _ => ()
     }
@@ -26,28 +33,28 @@ async fn main() -> Result<()> {
 
 
 async fn check_is_available(id: u32) -> Option<String> {
-    println!("{:?}", id);
-    let full: String = format!("{NETFLIX_ADDR}{id}");
+    // println!("{:?}", id);
+    let full_addr: String = format!("{NETFLIX_ADDR}{id}");
     let client = reqwest::Client::new();
     let res = client
-        .get(full)
+        .get(full_addr)
         .header(USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36")
         .send()
-        .await.unwrap();
-
-    if res.status() != StatusCode::OK {
+        .await;
+    if let Err(_) = &res {
         return None;
-    }
-
-    let header_map = res.headers();
-    println!("{:#?}", header_map);
-    let location = header_map.get("x-originating-url");
-    if let Some(v) = location {
-        let url = Url::parse(v.to_str().unwrap()).unwrap();
-        let location: Vec<&str> = url.path().split("/").map(|res| res).collect();
-        println!("{:?}", location);
-        // println!("{:?}", location[1].to_string());
-        return Some(location[1].to_string());
+    } else {
+        let res = res.unwrap();
+        if res.status() != StatusCode::OK {
+            return None;
+        }
+        let header_map = res.headers();
+        let location = header_map.get("x-originating-url");
+        if let Some(v) = location {
+            let url = Url::parse(v.to_str().unwrap()).unwrap();
+            let location: Vec<&str> = url.path().split("/").map(|res| res).collect();
+            return Some(location[1].to_string());
+        }
     }
     None
 }
